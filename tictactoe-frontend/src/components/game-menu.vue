3aedc234cf14548bd605a5c87e0f21de64eb59c3
GustavoKamradt <gutokamradt@gmail.com>
2024-06-19 20:43:11 -0300

    UI updated

--- tictactoe-frontend/src/components/game-menu.vue
+++ tictactoe-frontend/src/components/game-menu.vue
@@ -1,105 +1,129 @@
 <template>
-  <v-sheet width="100%" height="100%">
-  <v-container class="justify-center align-center fill-height pa-0">
-    <v-card class="pa-6" height="60%" width="90%" color="#1A237E" rounded="lg">
-      <v-card-title class="text-center">
-        <h2>JUBILADOS JUBILAM JUNTOS</h2>
-      </v-card-title>
-      <v-container class="fill-height flex-column">
-        <v-sheet color="#5C6BC0" class="pa-6" width="100%" elevation="5" rounded="lg">
-          <v-row justify="center">
-            <v-col class="text-center">
-              <h3>ESCOLHA SEU NOME NO JOGO:</h3>
-              <v-spacer></v-spacer>
-              <v-text-field label="nome de usuário" variant="underlined"></v-text-field>
+  <v-sheet width="100%" height="100%" theme="dark">
+    <v-container class="justify-center flex flex-column align-center fill-height pa-0">
+      <v-sheet class="justify-center align-center pa-6" style="aspect-ratio: 2/3" rounded="lg">
+        <v-container class="fill-height flex-column pa-0 justify-center align-center" style="max-width: ">
+          <v-card-title class="text-center mb-10">
+            <div class="text-md-h4 text-sm-h5  text-h6"> JUBILADOS JUBILAM JUNTOS</div>
+          </v-card-title>
+          <v-sheet id="v_sheet_name" class="pa-6" width="100%" elevation="5" rounded="lg">
+            <v-row justify="center">
+              <v-col class="text-center">
+                <div class="text-md-h5
+                text-sm-body-1
+                text-body-2">
+                  ESCOLHA SEU NOME NO JOGO:
+                </div>
+                <v-spacer></v-spacer>
+                <v-text-field label="nome de usuário" class="text-md-h4 text-body-2"
+                              variant="underlined"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-sheet>
+          <v-spacer></v-spacer>
+          <v-row class="justify-center pt-6 pb-3" no-gutters style="width: 100%">
+            <v-col style="height: 100%">
+              <v-btn id="menuButton"
+                     variant="flat"
+                     elevation="5"
+                     rounded="lg"
+                     width="100%"
+                     height="100%"
+                     class="cursor-pointer"
+                     @click="createNewGame">
+                <div class="text-md-h4
+                text-sm-h5
+                text-body-1">
+                  CRIAR PARTIDA
+                </div>
+              </v-btn>
             </v-col>
           </v-row>
-        </v-sheet>
-        <v-spacer></v-spacer>
-        <v-row justify="center" no-gutters style="width: 100%">
-          <v-col>
-            <v-btn size="x-large"
-                   rounded="lg"
-                   variant="flat"
-                   elevation="5"
-                   color="#5C6BC0"
-                   width="100%"
-                   @click="createNewGame">
-              CRIAR PARTIDA
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-row justify="center" no-gutters style="width: 100%">
-          <v-col cols="12">
-            <v-btn size="x-large"
-                   rounded="lg"
-                   variant="flat"
-                   elevation="5"
-                   color="#5C6BC0"
-                   width="100%"
-                   @click="createNewGame">
-              ENTRAR EM PARTIDA ALEATÓRIA
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-row justify="center" no-gutters style="width: 100%">
-          <v-col>
-            <v-dialog max-width="500"
-                      transition="dialog-bottom-transition"
-                      :persistent="otpDialog.persistence">
-              <template v-slot:activator="{ props: activatorProps }">
-                <v-btn size="x-large"
-                       rounded="lg"
-                       variant="flat"
-                       elevation="5"
-                       width="100%"
-                       color="#5C6BC0"
-                       v-bind="activatorProps">
+          <v-row class="justify-center pt-6 pb-3" no-gutters style="width: 100%">
+            <v-col cols="12">
+              <v-btn id="menuButton"
+                     variant="flat"
+                     elevation="5"
+                     rounded="lg"
+                     width="100%"
+                     height="100%"
+                     class="cursor-pointer"
+                     @click="createNewGame">
+                <div class="text-md-h4
+                text-sm-h5
+                text-body-1">
                   ENTRAR EM PARTIDA PRIVADA
-                </v-btn>
-              </template>
-              <template v-slot:default="{ isActive }">
-                <v-card
-                        color="#1A237E"
-                        rounded="lg"
-                elevation="5">
-                  <v-card-title class="text-center">
-                    <h2>INSIRA O SENHA DA PARTIDA</h2>
-                  </v-card-title>
-                  <v-card-text>
-                    <v-sheet color="#5C6BC0" class="pa-6" elevation="5" rounded="lg">
-                      <v-otp-input :loading="otpDialog.loading"
-                                   :model-value="otpDialog.code"
-                                   v-on:finish="checkOTPCode">
-                      </v-otp-input>
-                    </v-sheet>
-                  </v-card-text>
+                </div>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row class="justify-center pt-6 pb-3" no-gutters style="width: 100%">
+            <v-col>
+              <v-dialog max-width="500"
+                        transition="dialog-bottom-transition"
+                        :persistent="otpDialog.persistence">
+                <template v-slot:activator="{ props: activatorProps }">
+                  <v-btn id="menuButton"
+                         variant="flat"
+                         v-bind="activatorProps"
+                         elevation="5"
+                         rounded="lg"
+                         width="100%"
+                         height="100%"
+                         class="cursor-pointer"
+                  >
+                    <div class="text-md-h4
+                    text-sm-h5
+                    text-body-1">
+                      ENTRAR EM PARTIDA PRIVADA
+                    </div>
+                  </v-btn>
+                </template>
+                <template v-slot:default="{ isActive }">
+                  <v-card
+                    rounded="lg"
+                    elevation="5">
+                    <v-card-text>
+                      <v-row>
+                        <v-col class="justify-center align-center text-center">
+                          <div class="text-md-h4 text-sm-h5 text-body-1"> INSIRA A SENHA DA PARTIDA</div>
+                          <v-otp-input :loading="otpDialog.loading"
+                                       :model-value="otpDialog.code"
+                                       style="aspect-ratio: 10/1"
+                                       variant="underlined"
+                                       v-on:finish="checkOTPCode">
+                          </v-otp-input>
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
 
-                  <v-card-actions class="justify-center pa-6">
-                      <v-btn
-                        size="x-large"
-                        rounded="lg"
-                        variant="flat"
-                        elevation="5"
-                        color="#5C6BC0"
-                        width="100%"
-                        text="ENVIAR"
-                        @click="isActive.value = false"
-                      ></v-btn>
-                  </v-card-actions>
-                </v-card>
-              </template>
-            </v-dialog>
-          </v-col>
-        </v-row>
-        <v-spacer></v-spacer>
-      </v-container>
-    </v-card>
-  </v-container>
+                    <v-card-actions class="justify-center pa-6">
+                      <v-btn id="menuButton"
+                             @click="isActive.value = false"
+                      >
+                        <div class="text-md-h4
+                        text-sm-h5
+                        text-body-1
+                        cursor-pointer">
+                          ENVIAR
+                        </div>
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </v-col>
+          </v-row>
+          <v-spacer></v-spacer>
+        </v-container>
+      </v-sheet>
+
+    </v-container>
   </v-sheet>
 </template>
 
 <style>
+
 </style>
 
 <script setup>
@@ -113,7 +137,8 @@ var otpDialog = {
   code: '',
   persistence: false
 }
-function checkOTPCode(){
+
+function checkOTPCode() {
   otpDialog.loading = "loading";
   otpDialog.persistence = true;
 }
--- tictactoe-frontend/src/pages/game/[id].vue
+++ tictactoe-frontend/src/pages/game/[id].vue
@@ -1,91 +1,118 @@
 <template>
-  <v-container class="pt-8 min-h-screen min-w-screen">
-    <v-container class="justify-center">
-      <v-row class="justify-space-between align-center">
-        <v-col cols="auto" align-self="start">
-          <v-btn variant="tonal" color="red">Sair</v-btn>
-        </v-col>
-        <v-col cols="auto" align-self="end">
-          <v-label style="font-size: 5vw;">SENHA: {{game.otp}}</v-label>
-        </v-col>
-      </v-row>
+  <v-sheet width="100%" height="100%" theme="dark" class="pa-0">
+    <v-container class="justify-center flex flex-column align-center fill-height pa-0">
+      <v-container>
+        <v-container class="justify-center pa-0">
+          <v-row class="justify-space-between align-center">
+            <v-col cols="auto" align-self="start">
+              <v-btn variant="tonal" color="red" to="/">Sair</v-btn>
+            </v-col>
+            <v-col>
+              <v-dialog max-width="500px"
+                        transition="dialog-bottom-transition">
+                <template v-slot:activator="{props:activatorProps}">
+                  <v-btn variant="tonal"
+                         color="blue"
+                         v-bind="activatorProps"
+                         elevation="5"
+                         rounded="lg"
+                         class="cursor-pointer">
+                    <div>
+                      TESTE
+                    </div>
+                  </v-btn>
+                </template>
+                <template v-slot:default="{ isActive }">
 
+                </template>
+
+              </v-dialog>
+            </v-col>
+            <v-col cols="auto" align-self="end">
+              <v-label class="text-md-h4 text-sm-h5 text-body-1">SENHA: {{ game.otp }}</v-label>
+            </v-col>
+          </v-row>
+
+        </v-container>
+        <v-card>
+          <v-card-title class="text-center">
+            <h2 class="text-md-h4 text-sm-h5 text-body-1">ADVERSÁRIO</h2>
+          </v-card-title>
+          <v-row class="pa-7">
+            <v-col cols="auto" align-self="center">
+              <p class="text-md-h4 text-sm-h5 text-body-1">
+                {{ getOpponentName() == null ? 'Aguardando o oponente!' : getOpponentName() }}</p>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-container>
+
+      <v-spacer></v-spacer>
+
+      <v-container class="flex flex-column" style="aspect-ratio: 1/1; max-width: 50vh">
+        <v-row no-gutters>
+          <v-col cols="4"
+                 class="justify-center align-center"
+                 style="border-right: white 3px solid; border-bottom: white 3px solid; aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size:7.0vh">A</h1></v-btn>
+          </v-col>
+          <v-col cols="4"
+                 style="border-bottom: white 3px solid; aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size: 7vh">A</h1></v-btn>
+          </v-col>
+          <v-col cols="4"
+                 style="border-left: white 3px solid; border-bottom: white 3px solid; aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size:7.0vh">A</h1></v-btn>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="4"
+                 style="border-right: white 3px solid; aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size: 7.0vh;">A</h1></v-btn>
+          </v-col>
+          <v-col cols="4"
+                 style="aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size: 7.0vh;">A</h1></v-btn>
+          </v-col>
+          <v-col cols="4"
+                 style="border-left: white 3px solid; aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size: 7.0vh;">A</h1></v-btn>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="4"
+                 style="border-right: white 3px solid;border-top: white 3px solid;aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size: 7.0vh;">A</h1></v-btn>
+          </v-col>
+          <v-col cols="4"
+                 style="border-top: white 3px solid; aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size: 7.0vh;">A</h1></v-btn>
+          </v-col>
+          <v-col cols="4"
+                 style="border-left: white 3px solid; border-top: white 3px solid; aspect-ratio: 1/1; width: 33%">
+            <v-btn width="100%" height="100%" elevation="0"><h1 style="font-size: 7.0vh;">A</h1></v-btn>
+          </v-col>
+        </v-row>
+
+
+      </v-container>
+
+      <v-spacer></v-spacer>
+
+      <v-container>
+        <v-card>
+          <v-card-title class="text-center">
+            <h2 class="text-md-h4 text-sm-h5 text-body-1">VOCÊ</h2>
+          </v-card-title>
+          <v-row class="pa-7">
+            <v-col cols="auto">
+              <h2 class="text-md-h4 text-sm-h5 text-body-1">{{ playerName }}</h2>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-container>
     </v-container>
-    <v-card>
-      <v-card-title class="text-center">
-        <h2 style="font-size: 6vw;">ADVERSÁRIO</h2>
-      </v-card-title>
-      <v-row class="pa-7">
-        <v-col cols="auto" align-self="center">
-          <p style="font-size: 6vw;">{{ getOpponentName() == null ? 'Aguardando o oponente!' : getOpponentName() }}</p>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-container>
-
-  <v-spacer></v-spacer>
-
-  <v-container class="flex flex-column">
-    <v-row no-gutters>
-      <v-col cols="4"
-             class="justify-center align-center"
-             style="border-right: white 1px solid; border-bottom: white 1px solid; aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-      <v-col cols="4"
-             style="border-bottom: white 1px solid; aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-      <v-col cols="4"
-             style="border-left: white 1px solid; border-bottom: white 1px solid; aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="4"
-             style="border-right: white 1px solid; aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-      <v-col cols="4"
-             style="aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-      <v-col cols="4"
-             style="border-left: white 1px solid; aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="4"
-             style="border-right: white 1px solid;border-top: white 1px solid;aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-      <v-col cols="4"
-             style="border-top: white 1px solid; aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-      <v-col cols="4"
-             style="border-left: white 1px solid; border-top: white 1px solid; aspect-ratio: 1/1; width: 33%">
-        <v-btn width="100%" height="100%" style="font-size: 9vw;"><h1>A</h1></v-btn>
-      </v-col>
-    </v-row>
-
-  </v-container>
-
-  <v-spacer></v-spacer>
-
-  <v-container>
-    <v-card>
-      <v-card-title class="text-center">
-        <h2 style="font-size: 6vw;">VOCÊ</h2>
-      </v-card-title>
-      <v-row class="pa-7">
-        <v-col cols="auto">
-          <h2 style="font-size: 6vw;">{{ playerName }}</h2>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-container>
+  </v-sheet>
 </template>
 
 
